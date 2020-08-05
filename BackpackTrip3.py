0187eb0b981767a1af0019524cb877f9616f9f30
rednine13 <rednine13@gmail.com>
2020-08-05 22:26:09 +0900

    for loop order changed + input value in for loop

--- BackpackTrip3.py
+++ BackpackTrip3.py
@@ -1,7 +1,7 @@
-# import sys
-#
-# input = sys.stdin.readline
-# N, K = map(int, input().split())
+import sys
+
+input = sys.stdin.readline
+N, K = map(int, input().split())
 # items = [list(map(int, input().split())) for _ in range(N)]  # weight, value
 
 # N = 6
@@ -11,20 +11,19 @@
 # N = 4
 # K = 5
 # items = [[1, 4], [1, 8], [3, 3], [4, 8]]
+# # ans = 16
 
-N = 10
-K = 10
-items = [[1, 2], [4, 9], [1, 5], [4, 8], [4, 1], [1, 7], [3, 2], [3, 7], [2, 5], [5, 2]]
-
-# items.insert(0, [0, 0])
+# N = 10
+# K = 10
+# items = [[1, 2], [4, 9], [1, 5], [4, 8], [4, 1], [1, 7], [3, 2], [3, 7], [2, 5], [5, 2]]
 
 matrixDP = [[0 for col in range(K+1)] for row in range(N+1)] # 1~K, 1~N
-minWeight = min(items)[0]
 
-for w in range(minWeight, K+1):
-    for it in range(1, N+1):
-        if w - items[it-1][0] >= 0:
-            matrixDP[it][w] = max(matrixDP[it-1][w-items[it-1][0]] + items[it-1][1], matrixDP[it-1][w])
+for it in range(1, N + 1):
+    itemWeight, itemValue = map(int, input().split())
+    for w in range(1, K+1):
+        if w - itemWeight >= 0:
+            matrixDP[it][w] = max(matrixDP[it-1][w-itemWeight] + itemValue, matrixDP[it-1][w])
         else:
             matrixDP[it][w] = matrixDP[it-1][w]
 
