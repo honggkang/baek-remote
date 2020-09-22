467333c46c29202ec885f269698d5eb4c83d354e
rednine13 <rednine13@gmail.com>
2020-09-22 15:39:50 +0900

    marriage 2nd draft

--- StableMarriage.py
+++ StableMarriage.py
@@ -1,35 +1,13 @@
 # BOJ 3761
 
 import sys
-from queue import PriorityQueue  # further work
 
 input = sys.stdin.readline
 tn = int(input())
-n = int(input())
-names = list(input().split())  # male (name: lower) & female (name: upper)
-mName = names[0:n]
-fName = names[n:2*n]
-mName.sort()
-fName.sort()
 
-prefList = [[0]*n for _ in range(2*n)]
-# prefList[0][]~prfList[n-1][]: male pref. / prefList[n][]~prefList[2n-1][]: female pref.
-
-for _ in range(2*n):
-    temp = input()
-    idx = names.index(temp[0])
-    for p in range(n):
-        prefList[idx][p] = temp[p+2]
-
-mPrefList = prefList[0:n]
-fPrefList = prefList[n:2*n]
-
-boy = [False]*n  # if boy has his girl
-
-fQ = [[] for _ in range(n)]  # candidate of candidate
-fC = []*n  # candidate
-fR = [n]*n  # rank value initialize (rank: 0~n-1)
-
+ans_fName = []
+ans_mName = []
+ans_fC = []
 
 def matching(fpl, fq):
     l = len(fq)
@@ -42,18 +20,61 @@ def matching(fpl, fq):
             for li in range(l):
                 if fpl[i] == fq[li]:
                     return fq[li], i
-    else:
-        print('no matching')
 
 
-for i in range(n):
-    for fqi in range(n):
-        if fQ[fqi]:
-            c, r = matching(fPrefList[fqi], fQ[fqi])  # candidate and rank
-            if fR[fqi] > r:
-                fR[fqi] = r
-                fC[fqi] = c
-    if sum(boy) == n:
-        break
+for testi in range(tn):
+    n = int(input())
+    names = list(input().split())  # male (name: lower) & female (name: upper)
+    names.sort()
+    mName = names[n:2*n]
+    fName = names[0:n]
 
-print(fC)
+    prefList = [[0]*n for _ in range(2*n)]
+    # prefList[0][]~prfList[n-1][]: male pref. / prefList[n][]~prefList[2n-1][]: female pref.
+
+    for _ in range(2*n):
+        temp = input()
+        idx = names.index(temp[0])
+        for p in range(n):
+            prefList[idx][p] = temp[p+2]
+
+    mPrefList = prefList[n:2*n]
+    fPrefList = prefList[0:n]
+
+    boy = [False]*n  # if boy has his girl
+
+    fC = [[] for _ in range(n)]  # candidate
+    fR = [n]*n  # rank value initialize (rank: 0 ~ n-1)
+
+    bfi = [0]*n
+
+    while sum(boy) != n:
+        fQ = [[] for _ in range(n)]  # candidate of candidate
+        for j in range(n):
+            if not boy[j]:
+                i = bfi[j]
+                bfi[j] += 1
+                wc = mPrefList[j][i]  # wife candidate
+                fQ[fName.index(wc)].append(mName[j])
+
+        for fqi in range(n):
+            if fQ[fqi]:
+                c, r = matching(fPrefList[fqi], fQ[fqi])  # candidate and rank
+                if fR[fqi] > r:
+                    boy[mName.index(c)] = True
+                    # print(c)
+                    # print(fName[fqi])
+                    if fC[fqi]:
+                        boy[mName.index(fC[fqi])] = False
+                    fR[fqi] = r
+                    fC[fqi] = c
+
+    ans_fName.append(fName)
+    ans_mName.append(mName)
+    ans_fC.append(fC)
+
+
+for ti in range(tn):
+    for _ in range(n):
+        print(ans_mName[ti][_], ans_fName[ti][ans_fC[ti].index(mName[_])])
+    print('\n')
