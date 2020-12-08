b394b29ba2a76ec1be14d0d15793b633f4247bfa
rednine13 <rednine13@gmail.com>
2020-12-08 22:57:44 +0900

    BOJ 11725 memoization with GRAPH to reduce time complexity

--- TreeParent.py
+++ TreeParent.py
@@ -4,30 +4,31 @@ from collections import deque
 
 input = sys.stdin.readline
 
-# N = int(input())  # of nodes
-N = 7
+N = int(input())  # of nodes
+# N = 7
 p = {}
 Q = deque([1])  # default root
 
-# Mc = [list(map(int, input().split())) for _ in range(N-1)]  # group of linked nodes
-Mc = [[1, 6], [6, 3], [3, 5], [4, 1], [2, 4], [4, 7]]
+Mc = [list(map(int, input().split())) for _ in range(N-1)]  # group of linked nodes
+# Mc = [[1, 6], [6, 3], [3, 5], [4, 1], [2, 4], [4, 7]]
 Mct = [list(x) for x in zip(*Mc)]
 idx = 0
+Graph = {}
+for _ in range(1,N+1):
+    Graph[_] = []
+
+for _ in range(N-1):
+    x, y = Mc[_]
+    Graph[x].append(y)
+    Graph[y].append(x)
 
 while Q:
-    node = Q.popleft()
-    nu = Mct[0].count(node)+Mct[1].count(node)
-    for _ in range(nu):
-        try:
-            ni = Mct[idx].index(node)  # node index that has parent node 'node'
-        except ValueError:
-            idx = 1-idx
-            ni = Mct[idx].index(node)  # node index that has parent node 'node'
-        nv = Mct[1-idx][ni]  # node val. that has parent node 'node'
-        Mct[idx].pop(ni)
-        Mct[1-idx].pop(ni)
-        p[nv] = node
-        Q.append(nv)
+    pa_node = Q.popleft()
+    for ch_node in Graph[pa_node]:
+        p[ch_node] = pa_node
+        tdx = Graph[ch_node].index(pa_node)
+        Graph[ch_node].pop(tdx)
+        Q.append(ch_node)
 
 for _ in range(2, N+1):
     print(p[_])
